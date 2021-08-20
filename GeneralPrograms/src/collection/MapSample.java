6b58c10075dffff89b632390a1654ba79c9970c7
Ajay Singh <ajaysingh@mac.local>
2021-08-20 22:35:07 +0530

    Add MapSample

--- GeneralPrograms/src/collection/MapSample.java
+++ GeneralPrograms/src/collection/MapSample.java
@@ -1,4 +1,40 @@
 package collection;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapSample {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "Tango");
+        map.put(2, "Delta");
+        map.put(3, "Apache");
+        map.put(4, "Yanki");
+//
+//        for(Map.Entry<Integer, String> entry: map.entrySet()){
+//            System.out.println(entry.getKey() + "====" + entry.getValue());
+//        }
+//
+//        for(int i: map.keySet()){
+//            System.out.println(i + "===" + map.get(i));
+//        }
+
+        map.getOrDefault(5, "Police");
+
+        map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+        System.out.println("-----------------");
+
+        //not sorted
+        for(int i: map.keySet()){
+            System.out.println(i + "===" + map.get(i));
+        }
+    }
 }
+
+
